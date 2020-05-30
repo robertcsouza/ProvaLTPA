1a43b05679ab6c338a1baffa3556810426d65237
roberto <robertogeek18@gmail.com>
2020-05-30 01:21:28 -0400

    Updates

---
+++ precache-manifest.7d9beda484f37cc8d711cc1ed686f096.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc1135205a13d47eb7b9650fb267e4c",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "5602541998b85a549309",
+    "url": "/material-dashboard-react/static/css/2.5e1bfb3f.chunk.css"
+  },
+  {
+    "revision": "5e7a28dd25c3ecabd5b6",
+    "url": "/material-dashboard-react/static/css/main.1a5f11bd.chunk.css"
+  },
+  {
+    "revision": "5602541998b85a549309",
+    "url": "/material-dashboard-react/static/js/2.49c90b73.chunk.js"
+  },
+  {
+    "revision": "5e7a28dd25c3ecabd5b6",
+    "url": "/material-dashboard-react/static/js/main.92ae73b5.chunk.js"
+  },
+  {
+    "revision": "6a0cfeb8a7bb3dca7439",
+    "url": "/material-dashboard-react/static/js/runtime~main.21de15b8.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
